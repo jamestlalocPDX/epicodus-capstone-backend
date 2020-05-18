f466bd58c8e78a17fc4a76515aa1faea8946eb49
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-18 14:49:15 -0700

    add getPosts and createpost routes that grabs and adds data to the firestore collection 'posts'.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+// --- Software Development Kit (SDK) ---
+const admin = require ('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.getPosts = functions.https.onRequest((req, res) => {
+  admin.firestore().collection('posts').get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push(doc.data());
+      });
+      return res.json(posts);
+    })
+      .catch(err => console.error(err));
+});
+
+exports.createPost = functions.https.onRequest((req, res) => {
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+  .firestore()
+  .collection('posts')
+  .add(newPost)
+  .then(doc => {
+    res.json({ message: `document ${doc.id} created successfully` })
+  })
+  .catch((err) => {
+    res.status(500).json({ error: 'something went wrong' });
+    console.error(err);
+  })
+})
