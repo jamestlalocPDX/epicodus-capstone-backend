b2c88bcf3682ba9e2af225d23d90b059e53e3a4c
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-19 02:22:22 -0700

    move firebase config to its own file.

---
+++ functions/utility/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyADYr4zfxyYESkb574cznc0Njw4Drs82dY",
+  authDomain: "newco-5aacb.firebaseapp.com",
+  databaseURL: "https://newco-5aacb.firebaseio.com",
+  projectId: "newco-5aacb",
+  storageBucket: "newco-5aacb.appspot.com",
+  messagingSenderId: "946708994083",
+  appId: "1:946708994083:web:1be770fa0245de0a0cf996"
+};
