3f900b010cc9f37cfd734d30f193c2278d2a0f0e
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-19 15:03:56 -0700

    fix syntax error on line 88 and bug on line 83.

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -66,3 +66,32 @@ exports.getPost = (req, res) => {
       res.status(500).json({ error: err.code })
     })
 }
+
+exports.commentsOnPosts = (req, res) => {
+  if(req.body.body.trim() === '') {
+    return res.status(400).json({ error: "Please enter a comment."})
+  }
+
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    postId: req.params.postId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl
+  }
+
+  db.doc(`/posts/${req.params.postId}`).get()
+  .then(doc => {
+    if(!doc.exists) {
+      return res.status(400).json({ error: 'Post not found'})
+    }
+    return db.collection('comments').add(newComment);
+  })
+  .then(() => {
+    res.json(newComment)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ error: "Something went wrong" })
+  })
+}
