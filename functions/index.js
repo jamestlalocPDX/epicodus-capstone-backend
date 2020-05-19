7c29e5f7df9d6a5cbef9a3b3c8b7ebe1d4147d6d
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-18 23:54:02 -0700

    add login route that checks to see if the user input is valid and then authenticated.

--- functions/index.js
+++ functions/index.js
@@ -27,7 +27,7 @@ firebase.initializeApp(firebaseConfig)
 
 const db = admin.firestore();
 
-// ----Route that grabs posts data from firestore database collections----
+// <-------- GET ROUTE -------->
 app.get('/posts', (req, res) => {
   db
   .collection('posts')
@@ -48,7 +48,7 @@ app.get('/posts', (req, res) => {
       .catch(err => console.error(err));
 });
 
-// ----Route that creates posts data to firestore database collections----
+// <------- CREATE ROUTE -------->
 app.post('/post', (req, res) => {
   const newPost = {
     body: req.body.body,
@@ -68,7 +68,23 @@ app.post('/post', (req, res) => {
   })
 })
 
-// ----Sign-up route----
+const isEmpty = (string) => {
+  if(string.trim() == '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// <-------- SIGN-UP ROUTE -------->
 app.post('/signup', (req, res) => {
   const newUser = {
     email: req.body.email,
@@ -77,7 +93,34 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle
   };
 
-  db.doc(`/users/${newUser.handle}`).get()
+  let errors = {};
+// <---- Checks to see if email string is empty or has valid chars ---->
+  if(isEmpty(newUser.email)) {
+    errors.email = 'Please provide an email.';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Please enter a valid email address.';
+  }
+
+// <---- Checks to see if password is empty ---->
+  if(isEmpty(newUser.password)) {
+    errors.password = 'Please enter a password.';
+  }
+  if(newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = 'Both passwords must be the same!';
+  }
+
+// <----Checks to see if the user handle is empty ---->
+  if (isEmpty(newUser.handle)) {
+    errors.handle = "Please enter a user handle."
+  }
+
+  if(Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
     .then(doc => {
       if(doc.exists) {
         return res.status(400).json({ handle: 'this handle is already taken' });
@@ -88,10 +131,21 @@ app.post('/signup', (req, res) => {
       }
     })
     .then(data => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then(token => {
-      return res.status(201).json({ token });
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token })
     })
     .catch(err => {
       console.error(err);
@@ -101,7 +155,44 @@ app.post('/signup', (req, res) => {
         return res.status(500).json({ error: err.code });
       }
     })
+})
 
+// <---- LOG-IN ROUTE ---->
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+// <---- Checks to see if email string is empty ---->
+  if(isEmpty(user.email)) {
+    errors.email = "Please enter an email."
+  }
+
+// <---- Checks to see if password is empty ---->
+  if(isEmpty(user.password)) {
+    errors.password = "Please enter a password."
+  }
+
+// <----Checks to see if the user handle is empty ---->
+  if(Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
+// <----- Authenticates User ------>
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token })
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: error.code })
+    })
 })
 
 exports.api = functions.https.onRequest(app);
