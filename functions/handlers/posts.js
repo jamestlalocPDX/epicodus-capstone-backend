e528671f48c8687b6ea87a2b6a00edb27f33bf95
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-19 13:33:48 -0700

    fix syntax errors in getPost function.

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -38,3 +38,26 @@ exports.createPost = (req, res) => {
     console.error(err);
   })
 }
+
+exports.getPost = (req, res) => {
+  let postData = {};
+  db.doc(`/posts/${req.params.postId}`).get()
+    .then((doc) => {
+      if(!doc.exists) {
+        return res.status(404).json({ error: "Post not found." })
+      }
+      postData = doc.data();
+      postData.postId = doc.id;
+      return db.collection('comments').where('postId', '==', req.params.postId).get();
+    })
+    .then((data) => {
+      postData.comments = [];
+      data.forEach((doc) => {
+        postData.push(doc.data());
+      })
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code })
+    })
+}
