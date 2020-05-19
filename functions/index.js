added6fa163137ab0cdc8d902f2866d60daa719b
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-18 23:06:11 -0700

    add code to distinguish if an email is already in use due to a client error, or server error in signup route.

--- functions/index.js
+++ functions/index.js
@@ -95,7 +95,11 @@ app.post('/signup', (req, res) => {
     })
     .catch(err => {
       console.error(err);
-      return res.status(500).json({ error: err.code });
+      if(err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already in use' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
     })
 
 })
