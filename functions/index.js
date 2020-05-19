13cfe4f37633154193a30f3755641bcc39d9c3cd
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-19 01:04:36 -0700

    add authentication middleware function that checks for token authentication by intercepting requests using express.

--- functions/index.js
+++ functions/index.js
@@ -48,11 +48,40 @@ app.get('/posts', (req, res) => {
       .catch(err => console.error(err));
 });
 
-// <------- CREATE ROUTE -------->
-app.post('/post', (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No token found.')
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+
+  admin.auth().verifyIdToken(idtoken)
+  .then(decodedToken => {
+    req.user = decodedToken;
+    console.log(decodedToken);
+    return db.collection('users')
+      .where('userId', '==', req.user.uid)
+      .limit(1)
+      .get();
+  })
+  .then(data => {
+    req.user.handle = data.docs[0].data().handle;
+    return next();
+  })
+  .catch(err => {
+    console.error('Error while verifying token', err);
+    return res.status(403).json(err);
+  })
+}
+
+// <------- POST ROUTE -------->
+app.post('/post', FBAuth, (req, res) => {
   const newPost = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
